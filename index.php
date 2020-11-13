b4c4001daf9bbeb8705e3fe1a2748df9392ea75c
Valeriy Grechukha <v.mrfriz@gmail.com>
2020-11-13 15:23:31 +0300

    Добавлены закомментированные параметры php.ini (на всякий)

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
 
 namespace App;
+
 error_reporting(E_ALL);
 ini_set('short_open_tag', 'On');
+// ini_set('error_log', 'php-errors.log');
+// ini_set('html_errors', true);
+// ini_set('display_startup_errors', true);
+// ini_set('display_errors', true);
 
 include('vendor/autoload.php');
 require_once('db.php');
